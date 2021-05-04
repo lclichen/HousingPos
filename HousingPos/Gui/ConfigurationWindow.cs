0c67d8afc5f442d7435075c0f1d19225c2aa2d71
lclichen <lclichen@mail.ustc.edu.cn>
2021-05-04 13:35:25 +0800

    正确的Json发送方式。

--- HousingPos/Gui/ConfigurationWindow.cs
+++ HousingPos/Gui/ConfigurationWindow.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dalamud.Plugin;
-using Dalamud.Game.Text;
 using ImGuiNET;
 using HousingPos.Objects;
 using System.ComponentModel.DataAnnotations;
